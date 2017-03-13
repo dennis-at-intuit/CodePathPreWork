de969ed8baf68e8a31c27d5887a95026ffdf6fff
dlin1 <dennis_lin@intuit.com>
2017-03-12 20:49:01 -0700

    Project first commit.

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Lin, Dennis on 3/12/17.
+//  Copyright © 2017 Lin, Dennis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelTipAmount: UILabel!
+    @IBOutlet weak var labelTotalBill: UILabel!
+    @IBOutlet weak var textfieldBillAmount: UITextField!
+    @IBOutlet weak var segmentedControlTipPercentages: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func updateTotalBill2(_ sender: Any) {
+        updateTotalBill(sender)
+    }
+    
+    @IBAction func updateTotalBill(_ sender: Any) {
+        let arrayTipPercentages = [0.15, 0.18, 0.20, 0.25]
+        
+        let doubleBillAmount = Double(textfieldBillAmount.text!) ?? 0;
+        let doubleTipAmount = arrayTipPercentages[segmentedControlTipPercentages.selectedSegmentIndex] * doubleBillAmount
+        let doubleTotalBill = doubleBillAmount + doubleTipAmount
+        
+        labelTipAmount.text = String(format: "$%.2f", doubleTipAmount)
+        labelTotalBill.text = String(format: "$%.2f", doubleTotalBill)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true);
+    }
+}
+
